bfcd6ea739c1deea76a09c51d5c6161257bfcb3a
posthooks <113951213+posthooks@users.noreply.github.com>
2022-01-04 11:13:24 +0000

    Update Frameworks.md

--- evcxr/src/code_block.rs
+++ evcxr/src/code_block.rs
@@ -5,4 +5,17 @@
 // or https://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use crate::statemen
+use crate::statement_splitter;
+use anyhow::anyhow;
+use anyhow::Result;
+use once_cell::sync::OnceCell;
+use ra_ap_syntax::SyntaxNode;
+use regex::Regex;
+use statement_splitter::OriginalUserCode;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub(crate) struct Segment {
+    pub(crate) kind: CodeKind,
+    pub(crate) code: String,
+    num_lines: usize,
+    /// Only present for original user code. Provides ordering 
