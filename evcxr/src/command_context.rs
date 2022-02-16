573643bdbbf3f1319bcc021edd8539d4dd841441
posthooks <113951213+posthooks@users.noreply.github.com>
2022-02-16 19:41:57 +0000

    Testing Travis CI

--- evcxr/src/command_context.rs
+++ evcxr/src/command_context.rs
@@ -3,4 +3,18 @@
 // Licensed under the Apache License, Version 2.0 <LICENSE or
 // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE
 // or https://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed exc
+// copied, modified, or distributed except according to those terms.
+
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+use crate::code_block::CodeBlock;
+use crate::code_block::CodeKind;
+use crate::code_block::CommandCall;
+use crate::code_block::Segment;
+use crate::code_block::{self};
+use crate::crash_guard::CrashGuard;
+use crate::errors::bail;
+use crate::errors::CompilationError;
+use crate::error
