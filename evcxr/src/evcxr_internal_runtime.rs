5bc598c4e8d1e5e83e8a4eed1f8e1e27a69783c8
posthooks <113951213+posthooks@users.noreply.github.com>
2022-04-19 14:25:41 +0000

    Update sbt to 1.4.1

--- evcxr/src/evcxr_internal_runtime.rs
+++ evcxr/src/evcxr_internal_runtime.rs
@@ -1,3 +1,9 @@
 // Copyright 2020 The Evcxr Authors.
 //
-// Licensed under the Apac
+// Licensed under the Apache License, Version 2.0 <LICENSE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE
+// or https://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+// This file is both a module of evcxr and is included via include_str! then
+// built as a crate itself. The latter is the primary us
