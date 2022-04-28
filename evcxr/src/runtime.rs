703bfc9bb8bb9094387292942d5fd9caed4714ee
posthooks <113951213+posthooks@users.noreply.github.com>
2022-04-28 08:23:19 +0000

    fixes linting errors

--- evcxr/src/runtime.rs
+++ evcxr/src/runtime.rs
@@ -6,4 +6,15 @@
 // copied, modified, or distributed except according to those terms.
 
 use crate::errors::bail;
-us
+use crate::errors::Error;
+use once_cell::sync::OnceCell;
+use regex::Regex;
+use std::io;
+use std::marker::PhantomData;
+use std::rc::Rc;
+use std::{self};
+
+pub(crate) const EVCXR_IS_RUNTIME_VAR: &str = "EVCXR_IS_RUNTIME";
+pub(crate) const EVCXR_EXECUTION_COMPLETE: &str = "EVCXR_EXECUTION_COMPLETE";
+
+/// Binaries can call this just after staring. If w
