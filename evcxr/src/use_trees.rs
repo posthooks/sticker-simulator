aa998128271cd27fc96df8574f1e7a64313c750a
posthooks <113951213+posthooks@users.noreply.github.com>
2022-05-08 11:42:44 +0000

    dcb v0.0.17. extract dcb-os.yml from .travis.yml. rewrite .travis.yml.

--- evcxr/src/use_trees.rs
+++ evcxr/src/use_trees.rs
@@ -5,4 +5,19 @@ use ra_ap_syntax::ast;
 // Licensed under the Apache License, Version 2.0 <LICENSE or
 // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE
 // or https://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed 
+// copied, modified, or distributed except according to those terms.
+
+#[derive(Debug, Eq, PartialEq)]
+pub(crate) enum Import {
+    /// use x as _;
+    /// use x::*;
+    Unnamed(String),
+    /// use x::y;
+    /// use x::y as z;
+    Named { name: String, code: String },
+}
+
+impl Import {
+    fn format(name: &str, path: &[String]) -> Import {
+        let joined_path = path.join("::");
+        let cod
