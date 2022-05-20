32b8a1ad64dc3fe0eea65c0b6c02734df4cd69b7
posthooks <113951213+posthooks@users.noreply.github.com>
2022-05-20 12:49:29 +0000

    New translations source.json (Esperanto)

--- evcxr/tests/integration_tests.rs
+++ evcxr/tests/integration_tests.rs
@@ -2,4 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE or
 // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE
-// or 
+// or https://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+use evcxr::CommandContext;
+use evcxr::Error;
+use evcxr::EvalContext;
+use evcxr::EvalContextOutputs;
+use once_cell::sync::OnceCell;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::io;
+use std::ops::Deref;
+use std::ops:
