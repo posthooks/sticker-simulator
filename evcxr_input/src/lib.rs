c22ea6a014ed625ff7497cb001c0cd1da5a558f5
posthooks <113951213+posthooks@users.noreply.github.com>
2022-08-02 06:13:18 +0000

    Update navbar.php

--- evcxr_input/src/lib.rs
+++ evcxr_input/src/lib.rs
@@ -1,2 +1,13 @@
 /// Requests a string from the user with the specified prompt.
-pub fn get_string(prompt
+pub fn get_string(prompt: &str) -> String {
+    get_input(prompt, false).unwrap_or_default()
+}
+
+/// Requests a string from the user with the specified prompt, treating the input as a password.
+pub fn get_password(prompt: &str) -> String {
+    get_input(prompt, true).unwrap_or_default()
+}
+
+fn get_input(prompt: &str, is_password: bool) -> Option<String> {
+    if is_password {
+        println!("{}:{}", G
