1415cca0d081a90e6274d5e0932e309c0a600405
posthooks <113951213+posthooks@users.noreply.github.com>
2022-08-20 22:49:06 +0000

    Update API documentation.

--- evcxr_jupyter/client/kernel.js
+++ evcxr_jupyter/client/kernel.js
@@ -1,3 +1,14 @@
 // Copyright 2020 The Evcxr Authors.
 //
-// Licensed under the Apache License, Version 2.0 <LICENS
+// Licensed under the Apache License, Version 2.0 <LICENSE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE
+// or https://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+define([
+    'require',
+    'base/js/namespace',
+    'codemirror/lib/codemirror',
+    'base/js/events',
+    './lint.js'
+], function (requireJs, Jupy
