274a0e8feae6a22661441e2c66bbd3ec95ded566
posthooks <113951213+posthooks@users.noreply.github.com>
2022-08-21 06:43:51 +0000

    layers

--- evcxr_jupyter/client/kernel.js
+++ evcxr_jupyter/client/kernel.js
@@ -18,4 +18,13 @@ define([
         // It could be nice to show errors and warnings in the gutter as well.
         // We can enable that with the following line, however unfortunately
         // that messes up the horizontal scroll until the user clicks in editor.
-        // We can sort of fix that 
+        // We can sort of fix that by delaying setting of gutters by 1 second.
+        // That's too hacky though. We probably need to wait until some
+        // particular thing has been initialized. Until we figure out what that
+        // thing is, we leave the gutters off.
+
+        // cell.code_mirror.setOption('gutters', ["CodeMirror-lint-markers"])
+        cell.code_mirror.setOption('lint', true);
+    }
+
+   
