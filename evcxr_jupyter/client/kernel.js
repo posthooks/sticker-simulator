6cc8c1a386a141965f84d33523a631696554450c
posthooks <113951213+posthooks@users.noreply.github.com>
2022-08-21 06:35:04 +0000

    Add words

--- evcxr_jupyter/client/kernel.js
+++ evcxr_jupyter/client/kernel.js
@@ -11,4 +11,11 @@ define([
     'codemirror/lib/codemirror',
     'base/js/events',
     './lint.js'
-], function (requireJs, Jupy
+], function (requireJs, Jupyter, CodeMirror, events) {
+    "use strict";
+
+    function initCell(cell) {
+        // It could be nice to show errors and warnings in the gutter as well.
+        // We can enable that with the following line, however unfortunately
+        // that messes up the horizontal scroll until the user clicks in editor.
+        // We can sort of fix that 
