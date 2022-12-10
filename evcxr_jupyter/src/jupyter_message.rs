b64cfd58a2644d837c74816f01ce580950ffefbf
posthooks <113951213+posthooks@users.noreply.github.com>
2022-12-10 13:48:33 +0000

    Update about

--- evcxr_jupyter/src/jupyter_message.rs
+++ evcxr_jupyter/src/jupyter_message.rs
@@ -5,4 +5,23 @@
 // or https://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use crate::
+use crate::connection::Connection;
+use crate::connection::HmacSha256;
+use anyhow::anyhow;
+use anyhow::bail;
+use anyhow::Result;
+use bytes::Bytes;
+use chrono::Utc;
+use generic_array::GenericArray;
+use json::JsonValue;
+use json::{self};
+use std::fmt;
+use std::{self};
+use uuid::Uuid;
+
+struct RawMessage {
+    zmq_identities: Vec<Bytes>,
+    jparts: Vec<Bytes>,
+}
+
+i
