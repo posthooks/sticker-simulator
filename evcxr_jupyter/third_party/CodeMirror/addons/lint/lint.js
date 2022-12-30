69220e747ebc14f6a107f7a206c131bb29aa68dc
posthooks <113951213+posthooks@users.noreply.github.com>
2022-12-30 21:58:15 +0000

    hibernate

--- evcxr_jupyter/third_party/CodeMirror/addons/lint/lint.js
+++ evcxr_jupyter/third_party/CodeMirror/addons/lint/lint.js
@@ -10,4 +10,13 @@ function(CodeMirror) {
 
   function showTooltip(cm, e, content) {
     var tt = document.createElement("div");
-    tt.className = "CodeMirror-l
+    tt.className = "CodeMirror-lint-tooltip cm-s-" + cm.options.theme;
+    tt.appendChild(content.cloneNode(true));
+    if (cm.state.lint.options.selfContain)
+      cm.getWrapperElement().appendChild(tt);
+    else
+      document.body.appendChild(tt);
+
+    function position(e) {
+      if (!tt.parentNode) return CodeMirror.off(document, "mousemove", position);
+      tt.style.top = Math.max(0, e.clientY - tt.offsetHeight - 5) 
