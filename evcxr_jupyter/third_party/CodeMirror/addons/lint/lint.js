336166aa6c01b5a9bdcb341dd83c88afd297d13e
posthooks <113951213+posthooks@users.noreply.github.com>
2023-01-02 13:22:43 +0000

    fixes after rebase

--- evcxr_jupyter/third_party/CodeMirror/addons/lint/lint.js
+++ evcxr_jupyter/third_party/CodeMirror/addons/lint/lint.js
@@ -119,4 +119,15 @@ function(CodeMirror) {
     return tip;
   }
 
-  function lintAsyn
+  function lintAsync(cm, getAnnotations, passOptions) {
+    var state = cm.state.lint
+    var id = ++state.waitingFor
+    function abort() {
+      id = -1
+      cm.off("change", abort)
+    }
+    cm.on("change", abort)
+    getAnnotations(cm.getValue(), function(annotations, arg2) {
+      cm.off("change", abort)
+      if (state.waitingFor != id) return
+      if (arg2 && annotati
