27a798ed2d9d42ffedb10808754b74a463e391ae
posthooks <113951213+posthooks@users.noreply.github.com>
2023-01-02 06:42:01 +0000

    merging git tree

--- evcxr_jupyter/third_party/CodeMirror/addons/lint/lint.js
+++ evcxr_jupyter/third_party/CodeMirror/addons/lint/lint.js
@@ -107,4 +107,16 @@ function(CodeMirror) {
   }
 
   function annotationTooltip(ann) {
-    var severity = a
+    var severity = ann.severity;
+    if (!severity) severity = "error";
+    var tip = document.createElement("div");
+    tip.className = "CodeMirror-lint-message CodeMirror-lint-message-" + severity;
+    if (typeof ann.messageHTML != 'undefined') {
+      tip.innerHTML = ann.messageHTML;
+    } else {
+      tip.appendChild(document.createTextNode(ann.message));
+    }
+    return tip;
+  }
+
+  function lintAsyn
