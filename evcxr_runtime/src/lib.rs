cde54ad3f7ce4db3e9ba322882584b883719986d
posthooks <113951213+posthooks@users.noreply.github.com>
2023-02-20 11:35:31 +0000

    intermediate changes

--- evcxr_runtime/src/lib.rs
+++ evcxr_runtime/src/lib.rs
@@ -1,4 +1,13 @@
 // Copyright 2020 The Evcxr Authors.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE or
-// https://www.apache
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE
+// or https://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+#[cfg(feature = "bytes")]
+extern crate base64;
+
+pub trait Display {
+    /// Implementation should emit a representation of itself in one or mime
+    /// types  using the func
