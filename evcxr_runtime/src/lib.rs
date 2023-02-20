997b293fabe103bc568947097fa1fe6e14fabf55
posthooks <113951213+posthooks@users.noreply.github.com>
2023-02-20 16:27:28 +0000

    upgrade scalachess

--- evcxr_runtime/src/lib.rs
+++ evcxr_runtime/src/lib.rs
@@ -10,4 +10,17 @@ extern crate base64;
 
 pub trait Display {
     /// Implementation should emit a representation of itself in one or mime
-    /// types  using the func
+    /// types  using the functions below.
+    fn evcxr_display(&self);
+}
+
+/// Represents a mime type for some content that is yet to be emitted.
+pub struct ContentMimeType {
+    mime_type: String,
+}
+
+/// Prepares to output some content with the specified mime type.
+/// ```
+/// evcxr_runtime::mime_type("text/plain").text("Hello world");
+/// ```
+pub fn mime_type<S: Into<String>>(mim
