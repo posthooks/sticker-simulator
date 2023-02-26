73eef31ea240d48290a0bf020faa8b22e7b54922
posthooks <113951213+posthooks@users.noreply.github.com>
2023-02-26 20:58:30 +0000

    bump to 1.1

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,4 +8,16 @@ if [ -z "$VERSION" ]; then
   echo "RELEASE_NOTES.md doesn't contain an unreleased version" >&2
   exit 1
 fi
-if ! gi
+if ! git diff-index --quiet HEAD --; then
+  echo "Please commit all changes first" >&2
+  exit 1
+fi
+MIN_RUST_VER=$(grep ^rust-version evcxr/Cargo.toml | cut -d'"' -f2)
+if [ -z "$MIN_RUST_VER" ]; then
+  echo "Failed to determine minimum rust version" >&2
+  exit 1
+fi
+echo "Releasing $VERSION"
+git pull --rebase
+perl -pi -e 's/(^# .*) \(unreleased\)$/$1/' RELEASE_NOTES.md
+perl 
